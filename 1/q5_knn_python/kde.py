8be4a1b197e1e7d03700ed7c13e31e9f993ab3f5
Danial Hezarkhani <danial.hezarkhani@rwth-aachen.de>
2020-11-20 11:30:56 +0100

    kernel density estimation method with a Gaussian kernel implemented

--- 1/q5_knn_python/kde.py
+++ 1/q5_knn_python/kde.py
@@ -1,6 +1,7 @@
 import numpy as np
+import math as mth
 
-
+#kernel density estimation method with a Gaussian kernel with standard deviation h
 def kde(samples, h):
     # compute density estimation from samples with KDE
     # Input
@@ -8,7 +9,21 @@ def kde(samples, h):
     #  h          : (half) window size/radius of kernel
     # Output
     #  estDensity : estimated density in the range of [-5,5]
+    pos = np.arange(-5, 5.0, 0.1)  # Returns a 100 dimensional vector
 
-    #####Insert your code here for subtask 5a#####
-    # Compute the number of samples created
+    n = samples.shape[0]
+    d = pos.shape[0]
+
+    temp_value = []
+    for x in pos:
+        summe = 0
+        for j in range(0, n-1): # summe von 1 bis n
+            diff_norm = np.linalg.norm(x - samples[j])
+            summe += 1/(pow(2*mth.pi*pow(h,2),d/2))*mth.exp(-1*pow(diff_norm,2)/(2*pow(h,2)))
+        p_x = 1/n * summe
+        temp_value.append(p_x)
+
+    value = np.array(temp_value)
+    estDensity = np.column_stack((pos, value))
+
     return estDensity
